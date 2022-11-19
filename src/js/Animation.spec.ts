21cb60364a5217d317b35b27b2d767de4680d290
Frederik Schlüter <frederik.schlueter@posteo.de>
2022-11-19 16:25:54 +0100

    feat(Animation): reverse animations

--- src/js/Animation.spec.ts
+++ src/js/Animation.spec.ts
@@ -1,26 +1,79 @@
 import {} from "./mathUtils";
-import {CustomAnimation} from "./Animation";
-
-class MockCustomAmination extends CustomAnimation {
-    constructor(durationInSeconds: number, timeStampOnStartInSeconds: number) {
-        super(durationInSeconds, timeStampOnStartInSeconds);
-    }
-}
+import {CustomAnimation, Mode} from "./Animation";
 
 describe('Animation', () => {
     it('should return zero if calling run with t0', function () {
-        let animation = new MockCustomAmination(1, 0);
+        let animation = new CustomAnimation(1, 0);
 
         animation.run(0);
+
+        expect(animation.getProgress()).toBe(0);
+        expect(animation.isActivated).toBe(false);
+    });
+
+    it('should do nothing if not activated', function () {
+        let animation = new CustomAnimation(0, 0);
+
+        animation.run(123);
 
         expect(animation.getProgress()).toBe(0);
     });
 
     it('should return 1 if t is bigger than t1', function () {
-        let animation = new MockCustomAmination(0, 0);
+        let animation = new CustomAnimation(0, 0);
+        animation.isActivated = true;
 
         animation.run(123);
 
         expect(animation.getProgress()).toBe(1);
+    });
+
+    it('should return active if t >= 1', function () {
+        let animation = new CustomAnimation(0, 0);
+    animation.isActivated = true;
+
+        animation.run(123);
+
+        expect(animation.isActivated).toBe(true);
+    });
+
+    it('should be active and 1 if t = 0.5 and in loopmode', function () {
+        let animation = new CustomAnimation(3, 0, Mode.Loop);
+    animation.isActivated = true;
+
+        animation.run(1.5);
+
+        expect(animation.isActivated).toBe(true);
+        expect(animation.getProgress()).toBe(1);
+    });
+
+    it('should be active and 0 if t = t1 and in loopmode', function () {
+        let animation = new CustomAnimation(3, 0, Mode.Loop);
+        animation.isActivated = true;
+
+        animation.run(3);
+
+        expect(animation.isActivated).toBe(true);
+        expect(animation.getProgress()).toBe(0);
+    });
+
+    it('should be active and 1 if t = 1.5 * t1 and in loopmode', function () {
+        let animation = new CustomAnimation(3, 0, Mode.Loop);
+        animation.isActivated = true;
+
+        animation.run(4.5);
+
+        expect(animation.isActivated).toBe(true);
+        expect(animation.getProgress()).toBe(1);
+    });
+
+    it('should be active and 0.5 if t = 1.25 * t1 and in loopmode', function () {
+        let animation = new CustomAnimation(3, 0, Mode.Loop);
+        animation.isActivated = true;
+
+        animation.run(3.75);
+
+        expect(animation.isActivated).toBe(true);
+        expect(animation.getProgress()).toBe(0.5);
     });
 });
--- src/js/Animation.ts
+++ src/js/Animation.ts
@@ -1,15 +1,40 @@
+export enum Mode {
+    Loop,
+    Normal,
+    Paused,
+}
+
 export class CustomAnimation {
     private lastTimeStamp: number = 0;
-    constructor(private durationInSeconds: number, private timeStampOnStartInSeconds: number) {
+    isActivated: boolean = false;
+
+    constructor(private durationInSeconds: number, private timeStampOnStartInSeconds: number, private mode: Mode = Mode.Normal) {
     }
 
     run(currentTimeInSeconds: number) {
-        this.lastTimeStamp = (currentTimeInSeconds - this.timeStampOnStartInSeconds) / this.durationInSeconds;
-        if (this.lastTimeStamp > 1)
-            this.lastTimeStamp = 1;
+        if (this.isActivated === false)
+            return;
+        if (this.mode === Mode.Normal)
+        {
+            this.lastTimeStamp = (currentTimeInSeconds - this.timeStampOnStartInSeconds) / this.durationInSeconds;
+            if (this.lastTimeStamp > 1) {
+                this.lastTimeStamp = 1;
+                this.isActivated = true;
+            }
+        }
+        else if (this.mode === Mode.Loop) {
+            const t = (currentTimeInSeconds - this.timeStampOnStartInSeconds) % this.durationInSeconds;
+            this.lastTimeStamp = (2 * t) / this.durationInSeconds;
+            if (t > this.durationInSeconds / 2)
+                this.lastTimeStamp = 2 - this.lastTimeStamp;
+        }
     }
 
-    getProgress(): number{
+    getProgress(): number {
         return this.lastTimeStamp;
     }
+}
+
+export class AnimateLinearButton {
+
 }
